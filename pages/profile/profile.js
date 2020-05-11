aa2609384e395846d040577370d23516189580dc
150BigDream <2316624245@qq.com>
2020-05-11 15:13:05 +0800

    一丢丢

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -2,10 +2,23 @@
 //TODO 图片要改
 Page({
   data: {
+    userinfo:null,
     serviceList: [
-      { icon: 'cart.png', info: '提醒设置',url: 'remind' },//注意要有空格，才是string，否則是any
-      { icon: 'vip.png', info: '关于我们',url: 'about' }
+      { icon: 'cart.png', info: '提醒设置', url: 'remind' },//注意要有空格，才是string，否則是any
+      { icon: 'vip.png', info: '关于我们', url: 'about' }
     ]
+  },
+  login() {
+    wx.login({
+      complete: (res) => { },
+      fail: (res) => { },
+      success: (result) => {
+        this.setData.userinfo=wx.getUserInfo({
+          complete: (res) => {},
+        })
+       },
+      timeout: 0,
+    })
   },
   onLoad: function (options) {
 
